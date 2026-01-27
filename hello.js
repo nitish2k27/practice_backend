242390964672576f0cd3ce2bca3a3d763f2194dc
nitish2k27 <nitishpatil2k27@gmail.com>
2026-01-27 17:49:33 +0530

    the index file is updated with some add cart logic and the new html file is of rock paper scissor and .js file is full of logic for bith thr html files

--- hello.js
+++ hello.js
@@ -6,8 +6,46 @@
 // let j=24;
 // console.log(j+i);
 // console.log(process);
-console.log(process.env.PASSWORD);
+// console.log(process.env.PASSWORD);
 
-console.log("hello");
+// console.log("hello");
 
 
+let variable = 0;
+
+function cartcount() {
+    variable = variable + 1;
+    console.log(variable);
+}
+
+function reset(){
+    variable=0;
+    
+}
+
+function showquantity(){
+    console.log(variable);
+}
+
+function rockpaper(input) {
+    const arr = ['rock', 'paper', 'scissor'];
+
+    // pick random choice for computer
+    const comp = arr[Math.floor(Math.random() * arr.length)];
+
+    if (input === comp) {
+        console.log(`You and computer predicted ${input}. It's a draw.`);
+    }
+    else if (input === 'scissor' && comp === 'paper') {
+        console.log(`You chose ${input}, computer chose ${comp}. You won!`);
+    }
+    else if (input === 'rock' && comp === 'scissor') {
+        console.log(`You chose ${input}, computer chose ${comp}. You won!`);
+    }
+    else if (input === 'paper' && comp === 'rock') {
+        console.log(`You chose ${input}, computer chose ${comp}. You won!`);
+    }
+    else {
+        console.log(`You chose ${input}, computer chose ${comp}. You lost!`);
+    }
+}
